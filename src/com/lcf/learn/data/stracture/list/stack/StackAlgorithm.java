d22c1864799a1c10162cca088613c934eb72a3e7
liuchenfei <liuchenfei.lcf@foxmail.com>
2023-04-24 23:37:14 +0800

    stack algorithm

--- src/com/lcf/learn/data/stracture/list/stack/StackAlgorithm.java
+++ src/com/lcf/learn/data/stracture/list/stack/StackAlgorithm.java
@@ -15,9 +15,19 @@ public class StackAlgorithm {
 
     /**
      * 单调栈-每日温度
+     * 给定一个每天的温度列表，每天输出下一个更高温度在几天后
+     * [1,3,6,4,5,8,2] -> [0,1,2,2,3,6,1]
+     * 解法：单调栈，如果温度比栈顶低则入栈，比栈顶高则依次出栈里的元素，出的时候根据下标差计算值
      */
+    public static int[] lowerTemps(int[] temps) {
+
+    }
 
     /**
      * 接雨水
      */
+
+    public static void main(String[] args) {
+
+    }
 }
